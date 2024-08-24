b48cfe48636c32744df612753b4da7cbc15d34c8
uhammadziyo <DilafruzIminova71@gmail.com>
2024-08-24 09:03:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+let arr = ["non", "banan", "mashina","asal","olma","bori","gul"]
+let string = ["non", "banan", "mashina",]
+let number = ["asal","olma","bori","gul"]
+
+console.log( ${string}  `n li zozlar`);
+console.log( ${number}   `n ses zozlar`);
